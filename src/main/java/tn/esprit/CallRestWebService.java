e1c607aba71819ff950293539336d1707bdb9e26
farjallahnour <nour.farjallah@avaxia-group.com>
2022-12-28 11:31:30 +0100

    ma deuxième commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,7 +21,8 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 
-	public static final String endpoint = "http://ip-api.com/json";
+//2éme commit 	
+    public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
 		HttpClient client = new DefaultHttpClient();
